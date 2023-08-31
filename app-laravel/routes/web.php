08e27ec82f5d75780395a7945c764dd1a0fb024c
Carlos Ferreira <carlos@especializati.com.br>
2023-08-31 14:23:40 -0300

    feat: test trace apm with validation

--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
+use App\Http\Requests\TestFormRequest;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/test2', function (TestFormRequest $request) {
+    dd($request->all());
+});
 
 Route::get('/', function () {
     return view('welcome');
